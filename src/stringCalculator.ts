c7d6bc26af9554674b0380d74d8a6980589acdd0
Rishabh <rishabh.sikka@outlook.com>
2025-03-14 17:04:08 +0530

    feat: handle multiple number inputs

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,7 @@
 export const add = (numbers: string): number => {
   if (numbers.length === 0) return 0;
-  return parseInt(numbers);
+
+  const numArray = numbers.split(",").map((num) => Number(num));
+
+  return numArray.reduce((sum, num) => sum + num, 0);
 };
